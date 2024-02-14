32772a74883a406c720ab4a68b37572e80b4692e
notdule <148584497+notdule@users.noreply.github.com>
2024-02-14 23:59:09 +0100

    Update/Organize FollowPlayer.cs to be more Readable

--- Scripts/FollowPlayer.cs
+++ Scripts/FollowPlayer.cs
@@ -6,12 +6,9 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     [SerializeField] private Vector3 offset = new Vector3 (0, 5, -11);
-    // Start is called before the first frame update
     
-    // Update is called once per frame
     void LateUpdate()
     {
-        // Offset the Camera behind the player
         transform.position = player.transform.position + offset;
     }
 }
